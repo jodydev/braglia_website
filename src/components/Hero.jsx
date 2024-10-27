4cc21c3e921967f7283f5f8a44f93363ecde9b44
Jody Ossino <130100774+jodydev@users.noreply.github.com>
2024-10-27 18:36:33 +0100

    mobile fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,9 @@ import Image from "../assets/images/hero/hero.png";
 
 const Hero = () => {
   const { t } = useTranslation();
+  const isIOS = /iPad|iPhone|iPod/.test(navigator.platform);
 
+  console.log(isIOS);
   return (
     <section
       id="hero"
@@ -27,7 +29,7 @@ const Hero = () => {
         </main>
       </div>
 
-      <div className="w-4/5 sm:w-3/5 md:w-2/5 2xl:w-1/4 3xl:w-1/3 z-50 absolute bottom-[14%] sm:bottom-32 md:bottom-5 lg:bottom-10 right-11 sm:right-36 md:right-5 2xl:right-20 3xl:bottom-32 flex justify-center items-center text-white">
+      <div className="w-4/5 sm:w-3/5 md:w-2/5 2xl:w-1/4 3xl:w-1/3 z-50 absolute bottom-[25%] sm:bottom-32 md:bottom-5 lg:bottom-10 right-11 sm:right-36 md:right-5 2xl:right-20 3xl:bottom-32 flex justify-center items-center text-white">
         <div className="flex flex-col space-y-3 md:space-y-5 xl:space-y-8 w-full px-4 md:px-0">
           {/* Catalog button */}
           <div className="md:hidden flex flex-row border-2 border-white text-center py-2 md:py-3 px-4 md:px-10 lg:px-20 text-white hover:bg-white hover:text-primary cursor-pointer transition-all duration-700 ease-in-out">
@@ -60,7 +62,7 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className="w-full absolute bottom-3 md:bottom-0 lg:bottom-0 left-0 right-0 flex justify-center items-center p-4 md:p-10 text-white">
+      <div className="w-full absolute bottom-20 md:bottom-0 lg:bottom-0 left-0 right-0 flex justify-center items-center p-4 md:p-10 text-white">
         <div className="z-50 flex-col space-y-4 md:space-y-10">
           <div className="border-2 border-white rounded-full text-center p-2 md:p-3 text-white hover:bg-white hover:text-primary hover:cursor-pointer transition-all duration-1000 ease-in-out">
             <Link to="#category" className="text-base md:text-2xl ">
